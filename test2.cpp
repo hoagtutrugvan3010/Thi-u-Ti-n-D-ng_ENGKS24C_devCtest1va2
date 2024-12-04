20835bdf84b916dccc4608606a0e0209849b8980
hoagtutrugvan3010 <thieutiendung3010@gmail.com>
2024-12-04 12:20:17 +0700

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -1,65 +1,150 @@
-#include<stdio.h>
-#define vac 100
-int main(){
-	 int arr[vac][vac]; 
-    int n, l;   
-    int choice;  
-	do{
-	
-	
-	printf("MENU\n");
-		printf("1.nhap kich co va gia tri  cac phan tu cua mang\n");
-		printf("2.in gia tri cac phan tu cua mang theo ma tran\n");
-		printf("3.in ra  cac phan tu nam tren duong bien va tinh tich\n");
-		printf("4.in ra cac phan tu nao tren duong cheo chinh\n");
-		printf("5.in ra cac phan tunam tren duong cheo phu\n");
-		printf("6.tim kiem  1 phan tu  va in ra vi tri  cua phan tu do trong mang\n");
-		printf("7.sap xep  duong cheo chinh teheo thu tu tang dan\n");
-		printf("8.ket thuc\n");
-		scanf("%d",&choice);
-	
-		
-			
-		
-    
-    
-	   
-	    	 
-	    
-		
-	if(choice==1){
-		   
-				    printf("nhap so luong hang: ");
-				    scanf("%d", &n);
-				    printf("nhap so luong cot: ");
-				    scanf("%d", &l);
-				    printf("nhap cac gia tri cho mang :\n");
-				    for (int i = 0; i < n; i++) {
-				        for (int j = 0; j < l; j++) {
-				            printf("nhap phan tu thu [%d][%d]: ", i, j);
-				            scanf("%d", &arr[i][j]);
-				        }
-				    }
-				}
-				
-	if(choice==2){
-				    for(int i=0;i<n;i++){
-				    	for(int j=0;j<l;j++){
-				    		printf("%d\t",arr[i][j]);
-				    		
-						}
-						printf("\n");
-					}
-				}
-	if(choice==3){
-					for(int i=0;i<l;i++);
-					printf("%d\t",arr[0][i]);
-	}
-				
-					
-				} while(choice!=8);
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
 
- return 0;
- }
+// Hàm nhập mảng 2D
+void nhapMang(vector<vector<int>>& arr, int& n, int& m) {
+    cout << "Nhập số dòng: ";
+    cin >> n;
+    cout << "Nhập số cột: ";
+    cin >> m;
+    arr.resize(n, vector<int>(m));
+    cout << "Nhập các phần tử của mảng:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << "arr[" << i << "][" << j << "] = ";
+            cin >> arr[i][j];
+        }
+    }
+}
+
+// Hàm in mảng dưới dạng ma trận
+void inMang(const vector<vector<int>>& arr, int n, int m) {
+    cout << "Mảng dưới dạng ma trận:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Hàm in phần tử trên biên và tính tích
+void inBienTinhTich(const vector<vector<int>>& arr, int n, int m) {
+    long long tich = 1;
+    cout << "Các phần tử trên đường biên: ";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                cout << arr[i][j] << " ";
+                tich *= arr[i][j];
+            }
+        }
+    }
+    cout << "\nTích của các phần tử trên biên: " << tich << endl;
+}
+
+// Hàm in đường chéo chính
+void inCheoChinh(const vector<vector<int>>& arr, int n, int m) {
+    cout << "Các phần tử trên đường chéo chính: ";
+    for (int i = 0; i < min(n, m); i++) {
+        cout << arr[i][i] << " ";
+    }
+    cout << endl;
+}
+
+// Hàm in đường chéo phụ
+void inCheoPhu(const vector<vector<int>>& arr, int n, int m) {
+    cout << "Các phần tử trên đường chéo phụ: ";
+    for (int i = 0; i < min(n, m); i++) {
+        cout << arr[i][m - i - 1] << " ";
+    }
+    cout << endl;
+}
+
+// Hàm sắp xếp đường chéo chính tăng dần
+void sapXepCheoChinh(vector<vector<int>>& arr, int n, int m) {
+    vector<int> cheoChinh;
+    for (int i = 0; i < min(n, m); i++) {
+        cheoChinh.push_back(arr[i][i]);
+    }
+    sort(cheoChinh.begin(), cheoChinh.end());
+    for (int i = 0; i < min(n, m); i++) {
+        arr[i][i] = cheoChinh[i];
+    }
+    cout << "Đường chéo chính sau khi sắp xếp tăng dần.\n";
+}
+
+// Hàm tìm kiếm phần tử
+void timKiem(const vector<vector<int>>& arr, int n, int m) {
+    int x;
+    cout << "Nhập phần tử cần tìm: ";
+    cin >> x;
+    bool found = false;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == x) {
+                cout << "Phần tử " << x << " được tìm thấy tại vị trí (" << i << ", " << j << ")\n";
+                found = true;
+            }
+        }
+    }
+    if (!found) {
+        cout << "Phần tử " << x << " không tồn tại trong mảng.\n";
+    }
+}
+
+int main() {
+    vector<vector<int>> arr;
+    int n = 0, m = 0;
+    int choice;
+
+    do {
+        cout << "\nMENU\n";
+        cout << "1. Nhập kích cỡ và giá trị các phần tử của mảng\n";
+        cout << "2. In giá trị các phần tử của mảng theo ma trận\n";
+        cout << "3. In ra các phần tử nằm trên đường biên và tính tích\n";
+        cout << "4. In ra các phần tử nằm trên đường chéo chính\n";
+        cout << "5. In ra các phần tử nằm trên đường chéo phụ\n";
+        cout << "6. Sắp xếp đường chéo chính theo thứ tự tăng dần\n";
+        cout << "7. Tìm kiếm một phần tử và in ra vị trí của phần tử đó trong mảng\n";
+        cout << "8. Thoát\n";
+        cout << "Lựa chọn của bạn: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                nhapMang(arr, n, m);
+                break;
+            case 2:
+                inMang(arr, n, m);
+                break;
+            case 3:
+                inBienTinhTich(arr, n, m);
+                break;
+            case 4:
+                inCheoChinh(arr, n, m);
+                break;
+            case 5:
+                inCheoPhu(arr, n, m);
+                break;
+            case 6:
+                sapXepCheoChinh(arr, n, m);
+                break;
+            case 7:
+                timKiem(arr, n, m);
+                break;
+            case 8:
+                cout << "Chương trình kết thúc.\n";
+                break;
+            default:
+                cout << "Lựa chọn không hợp lệ. Vui lòng chọn lại.\n";
+        }
+    } while (choice != 8);
+
+    return 0;
+}
+
 
